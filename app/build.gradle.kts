26686660700180345b9358d998e347f116b8f6ad
Alec Ames <dev@alecames.com>
2026-08-28 18:33:12 -0400

    chore: bump version to v1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 30
         targetSdk = 35
         versionCode = 6
-        versionName = "1.4"
+        versionName = "1.5"
         vectorDrawables {
             useSupportLibrary = true
         }
